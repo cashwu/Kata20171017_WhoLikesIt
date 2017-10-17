1f652e208e10a87dcb654a699244c21c01c63e50
cash <cash@cashwu.com>
2017-10-17 15:28:18 +0800

    input 4 name

--- Kata20171017_WhoLikesIt/WhoLikesItTests.cs
+++ Kata20171017_WhoLikesIt/WhoLikesItTests.cs
@@ -19,7 +19,7 @@ namespace Kata20171017_WhoLikesIt
         {
             var kata = new Kata();
             var result = kata.Likes(new[] { "cash" });
-            Assert.AreEqual("cash Likes this", result);
+            Assert.AreEqual("cash likes this", result);
         }
 
         [TestMethod]
@@ -27,16 +27,24 @@ namespace Kata20171017_WhoLikesIt
         {
             var kata = new Kata();
             var result = kata.Likes(new[] { "cash", "cash1" });
-            Assert.AreEqual("cash and cash1 Likes this", result);
+            Assert.AreEqual("cash and cash1 likes this", result);
         }
 
         [TestMethod]
         public void Input_cash_cash1_cash2_should_return_cash_cash1_cash2_likes_this()
         {
             var kata = new Kata();
-            var result = kata.Likes(new[] { "cash", "cash1","cash2" });
-            Assert.AreEqual("cash, cash1 and cash2 Likes this", result);
-        } 
+            var result = kata.Likes(new[] { "cash", "cash1", "cash2" });
+            Assert.AreEqual("cash, cash1 and cash2 likes this", result);
+        }
+
+        [TestMethod]
+        public void Input_cash_cash1_cash2_cash3_should_return_cash_cash1_cash2_cash3_likes_this()
+        {
+            var kata = new Kata();
+            var result = kata.Likes(new[] { "cash", "cash1", "cash2", "cash3" });
+            Assert.AreEqual("cash, cash1 and 2 others likes this", result);
+        }
     }
 
     public class Kata
@@ -48,17 +56,22 @@ namespace Kata20171017_WhoLikesIt
                 return "no one likes this";
             }
 
+            if (name.Length == 1)
+            {
+                return $"{name[0]} likes this";
+            }
+
             if (name.Length == 2)
             {
-                return $"{name[0]} and {name[1]} Likes this";
+                return $"{name[0]} and {name[1]} likes this";
             }
 
             if (name.Length == 3)
             {
-                return $"{name[0]}, {name[1]} and {name[2]} Likes this";
+                return $"{name[0]}, {name[1]} and {name[2]} likes this";
             }
 
-            return $"{name[0]} Likes this";
+            return $"{name[0]}, {name[1]} and {name.Length - 2} others likes this";
         }
     }
 }
